3c10897ae048ac9f73c7d770c13e62d5b3d94cbb
DittyBOX <76231751+dittyBox@users.noreply.github.com>
2021-01-11 14:52:54 +0900

    add jwt

--- src/members/auth/auth.module.ts
+++ src/members/auth/auth.module.ts
@@ -3,14 +3,20 @@ import { AuthService } from './auth.service';
 import { MemberModule } from '../member.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [
-        MemberModule,
-        PassportModule],
-    controllers: [],
-    providers: [
-        AuthService,
-        LocalStrategy],
+  imports: [
+    MemberModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRETKEY, // env에 추가 필요 JWT_SECRETKEY: 아무거나
+      signOptions: { expiresIn: '60s' }, //토큰의 만료시간 60초
+    }),
+  ],
+  controllers: [],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
