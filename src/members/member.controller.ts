81da0221977eef5e5a1699e6bffaf03e46687cab
DittyBOX <76231751+dittyBox@users.noreply.github.com>
2021-01-11 14:57:16 +0900

    add jwt

--- src/members/member.controller.ts
+++ src/members/member.controller.ts
@@ -4,78 +4,83 @@ import { MemberInfo, AuthMemberInfo  } from "./member.type";
 import { MemberService } from "./member.service";
 import { CreateMemberDto } from './dto/create-member.dto';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller('members')
 export class MemberController {
+  constructor(
+    private readonly memberService: MemberService,
+    private authService: AuthService,
+  ) {}
 
-    constructor(private readonly memberService: MemberService){}
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Body() logins: LoginMemberDto): Promise<Response> {
+    const tokens = this.authService.login(logins.LOGIN_ID);
+    return new ResponseMessage().success().body(tokens).build();
+  }
 
-    @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async addMember(@Body() register: CreateMemberDto): Promise<Response> {
+    try {
+      // 인증된 사용자 인지 확인
+      //const authMember:AuthMemberInfo = await this.memberService.getAuthMember(register.AUTHMEMBERID);
+      //console.log(register.AUTHMEMBERID);
+      //if(!authMember){
+      //     return new ResponseMessage().error(9000,`권한오류 Memberid: ${register.AUTHMEMBERID}`).build();
+      //}
 
-    @Post('/login')
-    async login(@Request() req) {
-      return req.user;
+      const member: MemberInfo = await this.memberService.addMember(register);
+
+      if (!member) {
+        return new ResponseMessage()
+          .error(9001, `사용자 등록 실패 Login_id: ${register.LOGIN_ID}`)
+          .build();
+      }
+
+      return new ResponseMessage().success().body(member).build();
+    } catch (err) {
+      Logger.error(err);
     }
+  }
 
-    @Post()
-    async addMember(@Body() register: CreateMemberDto): Promise<Response> {
+  @Get('/:memberid')
+  async getMember(@Param('memberid') memberid: number): Promise<Response> {
+    try {
+      const member: MemberInfo = await this.memberService.getMember(memberid);
 
-        try{
+      if (!member) {
+        return new ResponseMessage()
+          .error(404, `Not found memberid : ${memberid}`)
+          .build();
+      }
 
-            //인증된 사용자 인지 확인
-            //const authMember:AuthMemberInfo = await this.memberService.getAuthMember(register.AUTHMEMBERID);
-            //console.log(register.AUTHMEMBERID);
-            //if(!authMember){
-           //     return new ResponseMessage().error(9000,`권한오류 Memberid: ${register.AUTHMEMBERID}`).build();
-            //}
-
-            const member:MemberInfo = await this.memberService.addMember(register);
-
-            if(!member){
-                return new ResponseMessage().error(9001,`사용자 등록 실패 Login_id: ${register.LOGIN_ID}`).build();
-            }
-
-            return new ResponseMessage().success().body(member).build();
-            
-        } catch(err){
-            Logger.error(err);
-        }
+      return new ResponseMessage().success().body(member).build();
+    } catch (err) {
+      Logger.error(err);
     }
+  }
 
-    @Get('/:memberid')
-    async getMember(@Param("memberid") memberid: number) : Promise<Response> {
+  @Delete('/:memberid')
+  async deleteMember(@Param('memberid') memberid: number): Promise<Response> {
+    try {
+      const member: MemberInfo = await this.memberService.getMember(memberid);
 
-        try{
-            const member:MemberInfo = await this.memberService.getMember(memberid);
+      if (!member) {
+        return new ResponseMessage()
+          .error(404, `Not found memberid : ${memberid}`)
+          .build();
+      }
 
-            if(!member){
-                return new ResponseMessage().error(404,`Not found memberid : ${memberid}`).build();
-            }
-
-            return new ResponseMessage().success().body(member).build();
-            
-        } catch(err){
-            Logger.error(err);
-        }
+      return new ResponseMessage().success().body(member).build();
+    } catch (err) {
+      Logger.error(err);
     }
+  }
 
-    @Delete('/:memberid')
-    async deleteMember(@Param("memberid") memberid: number) : Promise<Response>{
-        try{
-            const member:MemberInfo = await this.memberService.getMember(memberid);
-
-            if(!member){
-                return new ResponseMessage().error(404,`Not found memberid : ${memberid}`).build();
-            }
-
-            return new ResponseMessage().success().body(member).build();
-            
-        } catch(err){
-            Logger.error(err);
-        }
-    }
-
-/*     @Get('/:memberid')
+  /*     @Get('/:memberid')
     getTestValue(@Param("memberid") memberid: string) : string {
         return `<B><center><h1>너의 이름은 ${memberid} 입니다.</h1></center></B>`;
     } */
