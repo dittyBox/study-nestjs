9f7a35afe5e9f419795c86bacd9e8d4be6163db4
DittyBOX <76231751+dittyBox@users.noreply.github.com>
2021-01-11 14:55:15 +0900

    add login, use jwt

--- src/members/auth/auth.service.ts
+++ src/members/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { MemberService } from '../member.service';
 import { MemberInfo } from '../member.type';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private memberService: MemberService) {}
+  constructor(
+    private memberService: MemberService,
+    private jwtService: JwtService,
+  ) {}
 
-  //암호화 필요 
+  //암호화 필요
   //https://github.com/kelektiv/node.bcrypt.js#readme
   async validateMember(userid: string, password: string): Promise<any> {
     const user: MemberInfo = await this.memberService.loginMember(userid);
@@ -15,5 +19,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
